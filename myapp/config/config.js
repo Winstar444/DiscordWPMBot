de95d15bc2063adb9896ec274f247b9ee90d3fb7
externalPointerVariable <vicky986847@gmail.com>
2026-03-23 07:13:29 +0000

    Folder restructuring

--- myapp/config/config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
-
-module.exports = {
-    discordToken: process.env.DISCORD_BOT_TOKEN || "",
-    port: process.env.PORT || 4000
-};
